aa3a0f3d57d3598b871c653eeea453241209f0a3
Jeremy McDermond <nh6z@nh6z.net>
2015-12-15 10:17:36 -0800

    First testing version
    Test first version to check SPI transfers.

--- drivers/staging/adf4360/adf4360.c
+++ drivers/staging/adf4360/adf4360.c
@@ -14,61 +14,64 @@
 #include <linux/spi/spi.h>
 #include <linux/delay.h>
 
+#include "adf4360.h"
+
 struct adf4360_state {
 	struct spi_device				*spi;
 	struct adf4360_platform_data	*pdata;
-	u32								regs[3];
+	__be32							regs[3] ____cacheline_aligned;  // XXX be?
 	
-	__be32							val ____cacheline_aligned;
+	struct spi_transfer				xfers[3];
+	struct spi_message				message;
+	
 };
 
-static int adf4360_write_reg(struct adf4360_state *st, int reg) {
-	int ret;
-	
-	st->val = cpu_to_be32(st->regs[i] | (i & 0x03));
-	ret = spi_write(st->spi, &st->val, 3);
-	if(ret < 0)
-		return ret;
-	dev_err(&st->spi->dev, "[%d] 0x%X\n", i, (u32)st->regs[i] | (i & 0x03));  //  XXX Should be dev_dbg
-	
-	return 0;
+static int adf4360_sync_config(struct adf4360_state *st) {
+	return spi_sync(&st->spi, &st->message);
 }
 
-static int adf4360_sync_config(struct adf4360_state *st) {
-	int ret;
+static ssize_t rcounter_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count) {
+	struct ad4360_state *st = dev_get_drvdata(dev);
 	
-	ret = adf4360_write_reg(st, AD4360_R_REG);
-	if(ret < 0)
-		return ret;
-		
-	ret = adf4360_write_reg(st, AD4360_CONTROL_REG);
-	if(ret < 0)
-		return ret;
-		
-	msleep(10);
+	adf4360_sync_config(st);
 	
-	ret = adf4360_write_reg(st, AD4360_N_REG);
-	if(ret < 0)
-		return ret;
-		
-	return 0;
+	return count;
 }
+
+static DEVICE_ATTR(r-counter, S_IWUSR, NULL, rcounter_store);
 
 static int adf4360_probe(struct spi_device *spi) {
-	struct *adf4360_platform_data pdata;
+	struct adf4360_state *st;
+	int i;
 	
-	pdata = devm_kzalloc(dev, sizeof(*pdata), GFP_KERNEL);
-	if (!pdata)
-		return NULL;
+	// XXX need to deal with pdata here
+	
+	st = devm_kzalloc(dev, sizeof(*st), GFP_KERNEL);
+	if (!st)
+		return -ENOMEM;
+	
+	spi_set_drvdata(spi, st);
+	
+	st->spi = spi;
+	
+	spi_message_init(&st->message);
+	st->xfer[1].delay_usecs = 10000;
+	for(i = 0; i < 3; ++i) {
+		st->xfer[i].tx_buf = &st->regs[i];
+		st->xfer[i].len = 3;
+		st->xfer[i].cs_change = 1;
+		spi_message_add_tail(&st->xfer[i], &st->message);
+	}
+	
+	regs[0] = 0x00000001;
 
-	strncpy(&pdata->name[0], "ad4360", 32);
-	
-	spi_set_drvdata(spi, pdata);
-	
-	
+	sysfs_create_file(&spi->dev->kobj, &dev_attr_r-counter_name);
 }
 
 static int adf4360_remove(struct spi_device *spi) {
+	struct ad4360_state *st = spi_get_drvdata(spi);
+	
+	kfree(st);
 }
 
 static const struct of_device_id adf4360_of_id[] = {
